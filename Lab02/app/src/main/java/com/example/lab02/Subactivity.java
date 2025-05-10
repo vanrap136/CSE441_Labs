4a0eae1063d3e17cf6ada15f518c3a25988d2f30
vanrap136 <rap13062003@gmail.com>
2025-05-10 15:47:15 +0700

    6

--- Lab02/app/src/main/java/com/example/lab02/Subactivity.java
+++ Lab02/app/src/main/java/com/example/lab02/Subactivity.java
@@ -2,26 +2,17 @@ package com.example.lab02;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class subactivity extends AppCompatActivity {
+class subactivity extends AppCompatActivity {
     Button btnok;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sub);
+        setContentView(R.layout.activity_subactivity);
         btnok = findViewById(R.id.btnok);
         btnok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-// TODO Auto-generated method stub
-                finish();
-            }
         });
     }
 }
