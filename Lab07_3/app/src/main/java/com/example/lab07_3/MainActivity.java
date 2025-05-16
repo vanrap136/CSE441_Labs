4ddf6c1e0c483f828ab986e22d571c0fb2c39a7f
vanrap136 <rap13062003@gmail.com>
2025-05-16 16:27:34 +0700

    Lab07_3

--- Lab07_3/app/src/main/java/com/example/lab07_3/MainActivity.java
+++ Lab07_3/app/src/main/java/com/example/lab07_3/MainActivity.java
@@ -1,24 +1,52 @@
 package com.example.lab07_3;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtA, edtB, edtKQ;
+    Button btnrequest;
+    static final int REQUEST_CODE_SUB = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btnrequest = findViewById(R.id.btnrequest);
+
+        btnrequest.setOnClickListener(v -> {
+            Intent myintent = new Intent(MainActivity.this, SubActivity.class);
+            int a = Integer.parseInt(edtA.getText().toString());
+            int b = Integer.parseInt(edtB.getText().toString());
+            myintent.putExtra("soa", a);
+            myintent.putExtra("sob", b);
+            startActivityForResult(myintent, REQUEST_CODE_SUB);
         });
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SUB) {
+            if (resultCode == 33) {
+                int sum = data.getIntExtra("kq", 0);
+                edtKQ.setText("Tổng 2 số là: " + sum);
+            }
+            if (resultCode == 34) {
+                int sub = data.getIntExtra("kq", 0);
+                edtKQ.setText("Hiệu 2 số là: " + sub);
+            }
+        }
     }
 }
